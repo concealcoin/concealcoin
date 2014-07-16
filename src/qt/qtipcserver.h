ffae17bb58aeac81a6645777efe4405b58533cad
concealcoin <concealcoin@concealcoin.pw>
2014-07-17 00:03:06 +0400

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ConcealCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ConcealCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
